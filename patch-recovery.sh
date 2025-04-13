0d03d986e63163d3db5da01568e640c9129ac30d
Ravindu Deshan <droidcasts@protonmail.com>
2025-04-13 10:01:58 +0530

    scripts: enhance patch-recovery script with cleanup and unarchive functions

--- patch-recovery.sh
+++ patch-recovery.sh
@@ -10,14 +10,28 @@ export WDIR="$(pwd)"
 export RECOVERY_LINK="$1"
 mkdir -p "${WDIR}/recovery"
 
-# Downloading/copying the recovery
+# Clean-up is required
+rm -rf "${WDIR}/recovery/"*
 
-if [[ "${RECOVERY_LINK}" =~ ^https?:// ]]; then
-  curl -L "${RECOVERY_LINK}" -o "${WDIR}/recovery/$(basename "${RECOVERY_LINK}")"
-elif [ -f "${RECOVERY_LINK}" ]; then
-  cp "${RECOVERY_LINK}" "${WDIR}/recovery/"
-else
-  echo -e "Invalid input: not a URL or file.\n"
-  echo -e "If you entered a URL, make sure it begins with 'http://' or 'https://'"
-  exit 1
-fi
+# Downloading/copying the recovery
+download_recovery(){
+    if [[ "${RECOVERY_LINK}" =~ ^https?:// ]]; then
+    curl -L "${RECOVERY_LINK}" -o "${WDIR}/recovery/$(basename "${RECOVERY_LINK}")"
+    elif [ -f "${RECOVERY_LINK}" ]; then
+    cp "${RECOVERY_LINK}" "${WDIR}/recovery/"
+    else
+    echo -e "Invalid input: not a URL or file.\n"
+    echo -e "If you entered a URL, make sure it begins with 'http://' or 'https://'"
+    exit 1
+    fi
+}
+
+# Check if the downloaded/copied file an archive
+unarchive_recovery(){
+    cd "${WDIR}/recovery/"
+    local FILE=$(ls)
+    [[ "$FILE" == *.zip ]] && unzip "$FILE" && rm "$FILE"
+    [[ "$FILE" == *.lz4 ]] && lz4 -d "$FILE" "${FILE%.lz4}" && rm "$FILE"
+    mv "$(ls *.img)" "recovery.img"
+    cd "${WDIR}/"
+}
